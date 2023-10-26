5ca6c9a28efa7ddc2a076d7c00c5568b7ad4dd4f
Oksana581986 <oksanaotchych@gmail.com>
2023-10-26 14:44:35 +0300

    second_commit

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const startButton = document.querySelector("[data-start]");
+const stopButton = document.querySelector("[data-stop]");
+const body = document.body;
+
+let intervalId = null;
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+      }
+
+function startColorChange() {
+    startButton.disabled = true;
+    stopButton.disabled = false;
+    intervalId = setInterval(() => {
+      const randomColor = getRandomHexColor();
+      body.style.backgroundColor = randomColor;
+    }, 1000);
+  }
+
+  function stopColorChange() {
+    startButton.disabled = false;
+    stopButton.disabled = true;
+    clearInterval(intervalId);
+  }
+
+startButton.addEventListener('click', startColorChange);
+stopButton.addEventListener('click', stopColorChange);
 
